0c2968f21034288bb4f60a085ff584fe604fdb2a
Hos1miya <113900368+hos1miya@users.noreply.github.com>
2023-09-23 02:14:25 +0900

    stream切断対策

--- packages/frontend/src/stream.ts
+++ packages/frontend/src/stream.ts
@@ -6,3 +6,12 @@ import { url } from '@/config';
 export const stream = markRaw(new Misskey.Stream(url, $i ? {
 	token: $i.token,
 } : null));
+
+window.setTimeout(heartbeat, 1000 * 60);
+
+function heartbeat(): void {
+	if (stream != null && document.visibilityState === 'visible') {
+		stream.send('ping');
+	}
+	window.setTimeout(heartbeat, 1000 * 60);
+}
