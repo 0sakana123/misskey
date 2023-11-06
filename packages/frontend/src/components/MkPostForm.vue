6dae6c013061f4b43f3188241d795a768b2a3de9
Hos1miya <113900368+hos1miya@users.noreply.github.com>
2023-11-06 20:43:26 +0900

    x3, x4以外のMFMでノートを畳まないように

--- packages/frontend/src/components/MkPostForm.vue
+++ packages/frontend/src/components/MkPostForm.vue
@@ -580,11 +580,8 @@ async function post(ev?: MouseEvent) {
 	}
 
 	const annoying =
-		text.includes('$[x2') ||
 		text.includes('$[x3') ||
-		text.includes('$[x4') ||
-		text.includes('$[scale') ||
-		text.includes('$[position');
+		text.includes('$[x4');
 	if (annoying) {
 		const { canceled, result } = await os.actions({
 			type: 'warning',
